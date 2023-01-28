af21014f421b31b40d3921fa920baac0921733bd
Ajinkya Dawange <ajinkyadawange333ard@gmail.com>
2023-01-28 08:17:03 +0530

    semester page, branch page added

--- post/urls.py
+++ post/urls.py
@@ -9,13 +9,13 @@ from . import views
 # urls.py
 from django.urls import path
 # from .views import college_view, branch_view, semester_view, instructor_view, post_view,home
-from .views import home
+from .views import home,branch_view,semester_view
 
 urlpatterns = [
-    path('',home),
+    path('',home,name='home'),
     # path('college/<int:college_id>/', college_view, name='college_view'),
-    # path('branch/<int:branch_id>/', branch_view, name='branch_view'),
-    # path('semester/<int:semester_id>/', semester_view, name='semester_view'),
+    path('branch/<int:branch_id>/', branch_view, name='branch_view'),
+    path('semester/<int:semester_id>/', semester_view, name='semester_view'),
     # # path('course/<int:course_id>/', views.course_view, name='course_view'),
     # path('course/int:college_id/int:branch_id/int:semester_id/int:course_id/', views.course_view, name='course_view'),
     # path('instructor/<int:instructor_id>/', instructor_view, name='instructor_view'),
--- post/views.py
+++ post/views.py
@@ -19,27 +19,28 @@ def branch_view(request, branch_id):
     branch = get_object_or_404(Branch, id=branch_id)
     semesters = Semester.objects.filter(branch=branch)
     context = {'branch': branch, 'semesters': semesters}
-    return render(request, 'branch.html', context)
+    return render(request, 'post/branch.html', context)
 
-# def semester_view(request, semester_id):
-#     semester = get_object_or_404(Semester, id=semester_id)
-#     courses = Course.objects.filter(semester=semester)
-#     instructors = Instructor.objects.filter(course__semester=semester)
-#     context = {'semester': semester, 'courses': courses, 'instructors':instructors}
-#     return render(request, 'semester.html', context)
+def semester_view(request, semester_id):
+    semester = get_object_or_404(Semester, id=semester_id)
+    courses = Course.objects.filter(semester=semester)
+    courses1 = Course.objects.filter(semester=semester).first()
+    instructors = Instructor.objects.filter(course__semester=semester)
+    context = {'semester': semester, 'courses': courses,'courses1': courses1, 'instructors':instructors}
+    return render(request, 'post/semester.html', context)
 
 # def course_view(request, course_id):
 #     course = Course.objects.get(id=course_id)
 #     context = {'course': course}
-#     return render(request, 'course.html', context)
+#     return render(request, 'post/course.html', context)
 
 # def instructor_view(request, instructor_id):
 #     instructor = get_object_or_404(Instructor, id=instructor_id)
 #     context = {'instructor': instructor}
-#     return render(request, 'instructor.html', context)
+#     return render(request, 'post/instructor.html', context)
 
 # def post_view(request, post_id):
 #     post = get_object_or_404(PostForImage, id=post_id)
 #     context = {'post': post}
-#     return render(request, 'post.html', context)
+#     return render(request, 'post/post.html', context)
 
