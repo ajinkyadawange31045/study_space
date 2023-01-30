14b854e39773f864c994a06b7789ac3aa1e729af
Ajinkya Dawange <ajinkyadawange333ard@gmail.com>
2023-01-30 19:28:19 +0530

    instructor page added

--- post/urls.py
+++ post/urls.py
@@ -9,17 +9,14 @@ from . import views
 # urls.py
 from django.urls import path
 # from .views import college_view, branch_view, semester_view, instructor_view, post_view,home
-from .views import home,branch_view
+from .views import home,branch_view,instructor_view
 
 urlpatterns = [
     path('',home,name='home'),
     # path('college/<int:college_id>/', college_view, name='college_view'),
     path('branch/<int:branch_id>/', branch_view, name='branch_view'),
     path('courses/<int:course_id>/', views.course_view, name='course_view'),
-
-    # path('semester/<int:semester_id>/', semester_view, name='semester_view'),
-    # path('course/<int:course_id>/', views.course_view, name='course_view'),
-    # path('course/int:college_id/int:branch_id/int:semester_id/int:course_id/', views.course_view, name='course_view'),
-    # path('instructor/<int:instructor_id>/', instructor_view, name='instructor_view'),
+    
+    path('instructor/<int:instructor_id>/', instructor_view, name='instructor_view'),
     # path('post/<int:post_id>/', post_view, name='post_view'),
 ]
--- post/views.py
+++ post/views.py
@@ -1,6 +1,6 @@
 # views.py
 from django.shortcuts import render, get_object_or_404
-from .models import Branch, Semester, Course, Instructor,Instructor_post_pdf,Instructor_post_text
+from .models import Branch, Semester, Course, Instructor,Instructor_post_pdf,Instructor_post_text,Course_post
 from django.urls import reverse
 
 def home(request):
@@ -8,30 +8,7 @@ def home(request):
     branch = Branch.objects.all()
     # print(branch)
     context = {'branch':branch}
-    return render(request,'post/home.html',context)
-
-# def college_view(request, college_id):
-#     college = get_object_or_404(College, id=college_id)
-#     branches = Branch.objects.filter(college=college)
-#     context = {'college': college, 'branches': branches}
-#     return render(request, 'home/college.html', context)
-
-# def branch_view(request, branch_id):
-#     branch = get_object_or_404(Branch, id=branch_id)
-#     semesters = Semester.objects.filter(branch=branch)
-    
-#     # semester = get_object_or_404(Semester, id=semester_id)
-#     # courses = Course.objects.filter(semester=semester)
-#     # courses1 = Course.objects.filter(semester=semester).first()
-#     context = {'branch': branch, 'semesters': semesters,}
-#     return render(request, 'post/branch.html', context)
-
-# def branch_view(request, branch_id):
-#     branch = get_object_or_404(Branch, id=branch_id)
-#     semesters = Semester.objects.filter(branch=branch)
-#     courses = Course.objects.filter(semester__branch=branch)
-#     context = {'branch': branch, 'semesters': semesters, 'courses': courses}
-#     return render(request, 'post/branch.html', context)
+    return render(request,'post/home.html',context) 
 
 def branch_view(request, branch_id):
     branch = Branch.objects.get(id=branch_id)
@@ -46,26 +23,14 @@ def branch_view(request, branch_id):
 def course_view(request, course_id):
     course = get_object_or_404(Course, id=course_id)
     post_thing =Instructor.objects.filter(course=course)
-    context = {'course': course,'inc':post_thing}
+    course_post = Course_post.objects.filter(course=course)
+    context = {'course': course,'inc':post_thing,'course_post':course_post}
     return render(request, 'post/course_post.html',context)
 
-# def semester_view(request, semester_id):
-#     semester = get_object_or_404(Semester, id=semester_id)
-#     courses = Course.objects.filter(semester=semester)
-#     courses1 = Course.objects.filter(semester=semester).first()
-#     instructors = Instructor.objects.filter(course__semester=semester)
-#     context = {'semester': semester, 'courses': courses,'courses1': courses1, 'instructors':instructors}
-#     return render(request, 'post/semester.html', context)
-
-# def course_view(request, course_id):
-#     course = Course.objects.get(id=course_id)
-#     context = {'course': course}
-#     return render(request, 'post/course.html', context)
-
-# def instructor_view(request, instructor_id):
-#     instructor = get_object_or_404(Instructor, id=instructor_id)
-#     context = {'instructor': instructor}
-#     return render(request, 'post/instructor.html', context)
+def instructor_view(request, instructor_id):
+    instructor = get_object_or_404(Instructor, id=instructor_id)
+    context = {'instructor': instructor}
+    return render(request, 'post/instructor.html', context)
 
 # def post_view(request, post_id):
 #     post = get_object_or_404(Instructor_post_pdf,Instructor_post_text, id=post_id)
