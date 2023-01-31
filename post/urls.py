1e788b140856cd0f701a4ef1a3ad3d4248ec021e
Ajinkya Dawange <ajinkyadawange333ard@gmail.com>
2023-01-31 15:35:42 +0530

    urls set to slug and id, and designed 3 pages completely, now last 2 pages remaining to design and to link with front end

--- post/urls.py
+++ post/urls.py
@@ -15,10 +15,10 @@ urlpatterns = [
     path('',home,name='home'),
     # path('college/<int:college_id>/', college_view, name='college_view'),
     path('branch/<slug:url>/', semester, name='semester'),
-    path('branch/courses/<slug:url>/', views.course, name='course_view'),
+    path('<slug:branch_url>/<slug:semester_url>/courses/<slug:url>/', views.course, name='course_view'),
     
-    path('branch/courses/instructor/<slug:url>/', views.instructor, name='instructor_view'),
-    path('branch/courses/instructor/pdfs/<slug:url>/', views.instructor_post_pdf, name='pdf_view'),
-    path('branch/courses/instructor/post/<slug:url>/', views.instructor_post_text, name='post_view'),
+    path('<slug:branch_url>/<slug:semester_url>/courses/<slug:course_url>/instructor/<slug:url>/<int:course_taken_in_year>', views.instructor, name='instructor_view'),
+    path('<slug:branch_url>/<slug:semester_url>/courses/<slug:course_url>/instructor/<slug:instructor_url>/<int:course_taken_in_year>/pdf/<slug:pdf_url>/', views.instructor_post_pdf, name='pdf_view'),
+    path('<slug:branch_url>/<slug:semester_url>/courses/<slug:course_url>/instructor/<slug:instructor_url>/<int:course_taken_in_year>/post/<slug:post_url>/', views.instructor_post_text, name='post_view'),
     # path('post/<int:post_id>/', post_view, name='post_view'),
 ]
--- post/views.py
+++ post/views.py
@@ -20,27 +20,40 @@ def semester(request, url):
     return render(request, 'post/semester.html', context)
 
 
-def course(request, url):
+def course(request, url,branch_url,semester_url):
+    branch = Branch.objects.get(url = branch_url)
+    semester = Semester.objects.get(url = semester_url)
     course = get_object_or_404(Course, url=url)
     post_thing =Instructor.objects.filter(course=course)
     course_post = Course_post.objects.filter(course=course)
-    context = {'course': course,'inc':post_thing,'course_post':course_post}
+    context = {'branch':branch, 'course': course,'inc':post_thing,'course_post':course_post,'semester':semester}
     return render(request, 'post/course.html',context)
 
-def instructor(request, url):
-    instructor = get_object_or_404(Instructor, url=url)
-    instructor_post_pdf = Instructor_post_pdf.objects.all().filter(instructor=instructor)
-    instructor_post_text = Instructor_post_text.objects.all().filter(instructor=instructor)
-    context = {'instructor': instructor,'instructor_post_pdf':instructor_post_pdf,'instructor_post_text':instructor_post_text}
+def instructor(request, url, branch_url,semester_url,course_url,course_taken_in_year):
+    branch = Branch.objects.get(url = branch_url)
+    semester = Semester.objects.get(url = semester_url)
+    course = Course.objects.get(url = course_url)
+    instructor = get_object_or_404(Instructor, url=url,id=course_taken_in_year)
+    instructor_post_pdf1 = Instructor_post_pdf.objects.all().filter(instructor=instructor)
+    instructor_post_text1 = Instructor_post_text.objects.all().filter(instructor=instructor)
+    context = {'instructor': instructor,'instructor_post_pdf1':instructor_post_pdf1,'instructor_post_text1':instructor_post_text1,'course':course,'branch':branch,'semester':semester}
     return render(request, 'post/instructor.html', context)
 
-def instructor_post_pdf(request, url):
-    instructor_post_pdf = get_object_or_404(Instructor_post_pdf, url=url)
-    context = {'instructor_post_pdf': instructor_post_pdf}
+def instructor_post_pdf(request, pdf_url, branch_url,semester_url,course_url,instructor_url,course_taken_in_year):
+    branch = Branch.objects.get(url = branch_url)
+    semester = Semester.objects.get(url = semester_url)
+    course = Course.objects.get(url = course_url)
+    instructor = Instructor.objects.get(url=instructor_url,id=course_taken_in_year)
+    instructor_post_pdf1 = get_object_or_404(Instructor_post_pdf, url=pdf_url)
+    context = {'instructor_post_pdf1': instructor_post_pdf1,'course':course,'branch':branch,'semester':semester,'instructor':instructor}
     return render(request, 'post/instructor_post_pdf.html', context)
 
-def instructor_post_text(request, url):
-    instructor_post_text = get_object_or_404(Instructor_post_text, url=url)
-    context = {'instructor_post_text': instructor_post_text}
+def instructor_post_text(request, post_url, branch_url,semester_url,course_url,instructor_url,course_taken_in_year):
+    branch = Branch.objects.get(url = branch_url)
+    semester = Semester.objects.get(url = semester_url)
+    course = Course.objects.get(url = course_url)
+    instructor = Instructor.objects.get(url=instructor_url,id=course_taken_in_year)
+    instructor_post_text1 = get_object_or_404(Instructor_post_text, url=post_url)
+    context = {'instructor_post_text1': instructor_post_text1,'course':course,'branch':branch,'semester':semester,'instructor':instructor}
     return render(request, 'post/instructor_post_text.html', context)
 
